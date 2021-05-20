0f00ee77b42e8df8f47a775ffcb461c3edd8f940
gh0o5t <gh0o5t@protonmail.com>
2021-05-20 19:59:19 +0200

    Started GA

---
+++ modules/individual.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+class Individual():
+    def __init__(self, chromosome=[]):
+        self.chromosome = chromosome
+
+    def mutate(self):
+        pass
+
+    def fitness(self):
+        pass
+
+    def crossover(self):
+        pass
--- run.py
+++ run.py
@@ -9,13 +9,14 @@ def createScreen():
     game.setResolution()
     game.setBgColor()
     game.setScreen()
+    return game
+
+def randomPosition(margin):
+    pass 
 
 
-def main():
-    game = GameScreen()
-    game.setResolution()
-    game.setBgColor()
-    game.setScreen()
+def main_test():
+    game = createScreen()
     dest = GameObject((200,200), (20,20))
     dest.drawObject(game.screen)
     robot = Robot((300,300), (20,20), 20)
@@ -27,20 +28,29 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        robot.move(game.screenWidth, game.screenHeight, left=True)
+        robot.move(game.screenWidth, game.screenHeight, right=True)
         game.fillScreen()
         robot.drawObject(game.screen)
         dest.drawObject(game.screen)
         pygame.time.delay(100)
         pygame.display.flip()
-        robot.move(game.screenWidth, game.screenHeight, up=True)
-        game.fillScreen()
-        robot.drawObject(game.screen)
-        dest.drawObject(game.screen)
-        pygame.time.delay(100)
+        # robot.move(game.screenWidth, game.screenHeight, up=True)
+        # game.fillScreen()
+        # robot.drawObject(game.screen)
+        # dest.drawObject(game.screen)
+        # pygame.time.delay(100)
         pygame.display.flip()
     print(robot.x, robot.y)
         
+def main():
+    # create game sceen
+    game = createScreen()
+    
+    # create destination object
+    dest = GameObject 
+    
+    # create initial population
+    pass
 
 if __name__ == "__main__":
-    main()
+    main_test()
