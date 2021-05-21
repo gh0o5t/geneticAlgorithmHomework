21d8cb092e5e58f90ed87fd65111a5ce635cd9c1
gh0o5t <gh0o5t@protonmail.com>
2021-05-21 12:15:42 +0200

    Refactored the class

--- modules/game_screen.py
+++ modules/game_screen.py
@@ -2,15 +2,24 @@
 import pygame
 
 class GameScreen:
-    def __init__(self):
+    def __init__(
+            self,
+            screenWidth = 500,
+            screenHeight = 500,
+            bgColor = (0,0,0),
+        ):
         pygame.init()
-
-    def setResolution(self, screenWidth=500, screenHeight=500):
         self.screenWidth = screenWidth
         self.screenHeight = screenHeight
+        self.bgColor = bgColor
+        self.setScreen()
 
-    def setBgColor(self, color=(0,0,0)):
-        self.bgColor = color
+    # def setResolution(self, screenWidth: int, screenHeight: int):
+        # self.screenWidth = screenWidth
+        # self.screenHeight = screenHeight
+
+    # def setBgColor(self, color: tuple):
+        # self.bgColor = color
 
     def fillScreen(self, color=(0,0,0)):
         self.screen.fill(color) 
