28026a82197228ff52a41826cd603b7a67c468b2
gh0o5t <gh0o5t@protonmail.com>
2021-05-21 12:17:29 +0200

    Refactoring and some docs added

--- modules/game_object.py
+++ modules/game_object.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python3
 import pygame
+from modules.game_screen import GameScreen
 
 class GameObject:
-    def __init__(self, position, dimension):
+    def __init__(self, position: tuple, dimension: tuple):
         self.x = position[0]
         self.y = position[1]
         self.width = dimension[0]
         self.height = dimension[1]
         self.color = (255, 0, 0)
 
-    def setColor(self, color):
+    def setColor(self, color: tuple):
         self.color = color
 
-    def drawObject(self, screen):
+    def drawObject(self, screen: GameScreen):
         # screen must be a a GameScreen screen attribute
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
 
 class Robot(GameObject):
-    def __init__(self, position, dimension, velocity):
+    def __init__(self, position: tuple, dimension: tuple, velocity: int):
         GameObject.__init__(self, position, dimension)
         self.velocity = velocity
 
-    def move(self, wBorder, hBorder, up=False, down=False, left=False, right=False):
+    def move(self, wBorder: int, hBorder: int, direction: int):
+        """
+        :param direction: 1: left, 2, right, 3: up, 4: down
+        :type direction: int
+        """
         # moving left
-        if left and self.x > 0:
+        if direction == 1 and self.x > 0:
             self.x -= self.velocity
         # moving right
-        if right and self.x < wBorder - self.width:
+        if direction == 2 and self.x < wBorder - self.width:
             self.x += self.velocity
         # moving up
-        if up and self.y > 0:
+        if direction == 3 and self.y > 0:
             self.y -= self.velocity
         # moving down
-        if down and self.y < hBorder - self.height:
+        if direction == 4 and self.y < hBorder - self.height:
             self.y += self.velocity
 
